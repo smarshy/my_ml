815f8c0dda3c207b9f209813d06d5d91b3c3d7f3
Vatsala Swaroop <vatsala.swaroop@gmail.com>
2016-09-16 09:11:42 +0530

    Implemented backpropogation

--- digitRecognition_bp/nnCostFunction.m
+++ digitRecognition_bp/nnCostFunction.m
@@ -34,9 +34,34 @@ yVector = repmat([1:num_labels], m, 1) == repmat(y, 1, num_labels);
 J = sum(sum((-yVector.*log(a3) - (1-yVector).*log(1 - a3))))/m;
 
 % adding regularized cost
-J= J + (lambda/(2*m))*(sum(sum(Theta1(:,2:end).^2 ))+ sum(sum(Theta2(:,2:end).^2)))
+J= J + (lambda/(2*m))*(sum(sum(Theta1(:,2:end).^2 ))+ sum(sum(Theta2(:,2:end).^2)));
 
-% =========================================================================
+
+
+% implementing backpropogation
+delta1 = zeros(size(Theta1));
+delta2 = zeros(size(Theta2));
+
+for t=1:m
+	% for training sample t
+	a1_single_sample=a1(t,:);
+	a2_single_sample=a2(t,:);
+	a3_single_sample=a3(t,:);
+	yVector_single_sample=yVector(t,:);
+
+	del3=a3_single_sample-yVector_single_sample;
+
+	z2_single_sample = [1 a1_single_sample*Theta1'];
+	del2 = del3*Theta2 .* sigmoidGradient(z2_single_sample);
+
+	delta1 = delta1 + del2(2:end)' * a1_single_sample;
+	delta2 = delta2 + del3' * a2_single_sample;
+end
+
+% Added regularization
+Theta1_grad = (1 / m) * delta1 + (lambda / m) * [ zeros(size(Theta1, 1), 1) Theta1(:,2:end) ];
+Theta2_grad = (1 / m) * delta2 + (lambda / m) * [ zeros(size(Theta2, 1), 1) Theta2(:,2:end) ];
+
 
 % Unroll gradients
 grad = [Theta1_grad(:) ; Theta2_grad(:)];
