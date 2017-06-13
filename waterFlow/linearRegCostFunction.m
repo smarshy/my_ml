faea18e6a24814eaa42b1aa292a0045d6e631186
Vatsala Swaroop <vatsala.swaroop@gmail.com>
2017-06-14 00:40:03 +0530

    Added regularized cost and gradient implementation

--- waterFlow/linearRegCostFunction.m
+++ waterFlow/linearRegCostFunction.m
@@ -14,6 +14,13 @@ grad = zeros(size(theta));
 
 % =========================================================================
 
+% Computed the cost and gradient of regularized linear 
+% regression for a particular choice of theta
+
+% ignored theta(1) for regularization, also preserve dimensions
+J =(sum((X*theta-y).^2)+lambda*sum([0;theta(2:end)].^2))/(2*m);
+grad=(1/m).*(X'*(X*theta-y))+(lambda/m)*[0;theta(2:end)];
+
 % =========================================================================
 
 grad = grad(:);
